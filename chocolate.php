b9312b6ebe3e37de45cf767e9c553901518fc906
jayyjayy <164137318+jayyjayy@users.noreply.github.com>
2024-05-16 09:08:56 +0800

    Add files via upload

---
+++ chocolate.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHOCOLATE FACTORY</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .container{
+            text-align: center;
+            font-size: 30px;
+            float: left;
+            width: 30%;
+            padding: 5px;
+        }
+        body{
+            background-color: lightblue;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar">
+    <ul>
+        <li><a href="chocolate">CHOCOLATE</a></li>
+        <li><a href="create.php">ADD NEW PRODUCT?....CLICK HERE</a></li>
+    </ul>
+</nav>
+<div class="container">
+    <div class="box">
+         <img src=images/chocolate.jpg>
+         <h4>KITKAT</h4>
+         <h5>50.00</h5>
+    </div>
+    <div class="box">
+         <img src=images/chocolate1.jpg>
+         <h4>HERSHEY'S</h4>
+         <h5>250.00</h5>
+    </div>
+     <div class="box">
+         <img src=images/chocolate2.jpg>
+         <h4>Ghirardelli</h4>
+         <h5>200.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+</div>
+<div class="container">
+     <div class="box">
+         <img src=images/chocolate3.jpg>
+         <h4>product</h4>
+         <h5>150.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+     <div class="box">
+         <img src=images/chocolate4.jpg>
+         <h4>TOBLERONE</h4>
+         <h5>60.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+     <div class="box">
+         <img src=images/chocolate5.jpg>
+         <h4>SNIKERS</h4>
+         <h5>50.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart' ></i></a>
+         </div>
+      </div>
+</div>
+<div class="container">
+     <div class="box">
+         <img src=images/chocolate6.jpg>
+         <h4>product</h4>
+         <h5>100.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+     <div class="box">
+         <img src=images/chocolate7.jpg>
+         <h4>product</h4>
+         <h5>120.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+     <div class="box">
+         <img src=images/chocolate8.jpg>
+         <h4>Milka</h4>
+         <h5>90.00</h5>
+         <div class="cart"> 
+             <a href="#" i class='bx bx-cart'></i></a>
+         </div>
+      </div>
+</div>
+</body>
+</html>
